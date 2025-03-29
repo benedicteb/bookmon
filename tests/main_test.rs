684520f4299065d982b2bd21d958905a2795f61a
Benedicte Emilie Brækken <benedicte@kodeklang.no>
2025-03-29 13:10:26 +0100

    Add test file skeleton

---
+++ tests/main_test.rs
@@ -0,0 +1,14 @@
+#[test]
+fn test_hello_world() {
+    // This is a basic test that we can expand later
+    // Currently, we're just testing that the program compiles
+    assert!(true);
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_basic_arithmetic() {
+        assert_eq!(2 + 2, 4);
+    }
+} 
