412a64663023d66c5f0c51020e05dffa5a590516
GitHub Action <action@github.com>
2025-03-31 20:03:01 +0000

    chore: update formula version to v1.0.38

--- homebrew-bookmon/bookmon.rb
+++ homebrew-bookmon/bookmon.rb
@@ -1,7 +1,7 @@
 class Bookmon < Formula
   desc "A command-line tool for tracking your reading progress"
   homepage "https://github.com/benedicteb/bookmon"
-  url "https://github.com/benedicteb/bookmon.git", tag: "v1.0.37"
+  url "https://github.com/benedicteb/bookmon.git", tag: "v1.0.38"
 
   depends_on "rust" => :build
 
