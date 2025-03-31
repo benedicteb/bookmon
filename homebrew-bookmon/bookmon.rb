735dbcf066d445e961995445e7ba035547062b07
Benedicte Emilie Brækken <benedicte@kodeklang.no>
2025-03-31 19:58:45 +0200

    feat(homebrew): add homebrew formula

---
+++ homebrew-bookmon/bookmon.rb
@@ -0,0 +1,15 @@
+class Bookmon < Formula
+  desc "A command-line tool for tracking your reading progress"
+  homepage "https://github.com/benedicte/bookmon"
+  url "https://github.com/benedicte/bookmon.git", tag: "v0.1.0"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--root", prefix, "--path", "."
+  end
+
+  test do
+    system "#{bin}/bookmon", "--version"
+  end
+end 
