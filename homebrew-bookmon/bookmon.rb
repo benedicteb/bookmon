db75c8573446c2487caf526b2c8aec7d90500eb0
Benedicte Emilie Brækken <benedicte@kodeklang.no>
2025-03-31 22:04:07 +0200

    fix(homebrew): switch to ssh url

--- homebrew-bookmon/bookmon.rb
+++ homebrew-bookmon/bookmon.rb
@@ -1,7 +1,7 @@
 class Bookmon < Formula
   desc "A command-line tool for tracking your reading progress"
   homepage "https://github.com/benedicteb/bookmon"
-  url "https://github.com/benedicteb/bookmon.git", tag: "v1.0.38"
+  url "git@github.com:benedicteb/bookmon.git", tag: "v1.0.38"
 
   depends_on "rust" => :build
 
