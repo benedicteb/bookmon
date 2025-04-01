31500345d57e67feba82d262ee04ba4dffb47d96
Benedicte Emilie Brækken <benedicte@kodeklang.no>
2025-04-01 12:13:53 +0200

    fix(homebrew): hash sed edit line in-place
    Not add duplicate ones.

--- Formula/bookmon.rb
+++ Formula/bookmon.rb
@@ -2,7 +2,6 @@ class Bookmon < Formula
   desc "A command-line tool for tracking your reading progress"
   homepage "https://github.com/benedicteb/bookmon"
   url "https://github.com/benedicteb/bookmon/archive/refs/tags/v1.0.43.tar.gz"
-  sha256 "4330e59a1216e7a6ca524d8def4f50136f7bb4d16a17fdb7307c99e5d6ed28cc"
   sha256 "sha-placeholder"
 
   depends_on "rust" => :build
