3125a6b491ce914daa5b2e9b533f52f8fceb1aec
GitHub Action <action@github.com>
2026-02-21 14:19:41 +0000

    chore: update formula version to v1.0.66 and add SHA256 hash

--- Formula/bookmon.rb
+++ Formula/bookmon.rb
@@ -1,8 +1,8 @@
 class Bookmon < Formula
   desc "A command-line tool for tracking your reading progress"
   homepage "https://github.com/benedicteb/bookmon"
-  url "https://github.com/benedicteb/bookmon/archive/refs/tags/v1.0.65.tar.gz"
-  sha256 "e3686ad824b54ced99698d8f3400c4dbb1be5e7efb3de5e027f879943d703b74"
+  url "https://github.com/benedicteb/bookmon/archive/refs/tags/v1.0.66.tar.gz"
+  sha256 "69279f035916862e0e0dc240e2c2c607a459fea9ab35cd4dfce2dafd6879efe2"
 
   depends_on "rust" => :build
 
